9af2144dac38c371024b70ede4875178480e6216
KEEGAN LAUNSPACH <keegan.launspach1@hotmail.com>
2023-07-21 21:24:08 +0200

    cleanup and fixes to Framer Motion container

--- src/AnimatedPage.tsx
+++ src/AnimatedPage.tsx
@@ -5,9 +5,9 @@ type AnimatedPageProps = {
 
 const AnimatedPage = (props: AnimatedPageProps) => {
   const animations = {
-    initial: {opacity: 0, x: 100},
-    animate: {opacity: 1, x: 0},
-    exit: {opacity: 0, x: -100}
+    initial: {opacity: 0},
+    animate: {opacity: 1},
+    exit: {opacity: 0}
   }
   return (
     <motion.div 
