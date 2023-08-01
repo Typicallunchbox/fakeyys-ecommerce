ad06b2fe8c599b8245a144e95bc2b2417a455931
Keegan <keegan.launspach1@hotmail.com>
2023-08-01 09:43:12 +0200

    scroll fix

--- src/pages/catalogue.tsx
+++ src/pages/catalogue.tsx
@@ -1,6 +1,6 @@
 import AnimatedPage from "../AnimatedPage";
 import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 interface Product {
     id: number,
@@ -14,7 +14,37 @@ const catalogue = () => {
   const [selectedId, setSelectedId] = useState<string>('')
   const [selectedProduct, setSelectedProduct] = useState<Product | {}>({})
   const [openModal, setOpenModal] = useState<boolean>(false)
+  const [lockScroll, setLockScroll] = useState<boolean>(false);
 
+  useEffect(() => {
+    if(openModal) {
+      document.querySelector("body")!.classList.add('no-scroll');
+    }else{
+      document.querySelector("body")!.classList.remove('no-scroll');
+    }
+}, [lockScroll])
+
+  // Framer Animations
+  const transition = {duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9]}
+  const variants = {
+    load: {opacity: 1, transition: {duration: 0.5}},
+    show: {opacity: 1, transition: {duration: 0}},
+    hide: {opacity: 0,transition: {duration: 0.5}}
+  };
+  const textVariant = {
+    initial: {opacity: 1, transition: {duration: 0}},
+    show: {opacity: 0,transition: {duration: 0.5}},
+    hide: {opacity: 0,transition: {duration: 0.5}}
+  };
+  const variantsBackup = {
+    rotate: { rotate: [0, -30, 0], transition: { duration: 0.5 } },
+    // You can do whatever you want here, if you just want it to stop completely use `rotate: 0`
+    stop: { y: [0, -10, 0], transition: { repeat: Infinity, repeatDelay: 3 } },
+    initial: {opacity: 1},
+    hide: {opacity: 0, transition: transition}
+  };
+
+  //Test Payload Data
   const fakeyysProducts = [
     {
       id: '0',
@@ -83,18 +113,26 @@ const catalogue = () => {
   ]
   return (
     <AnimatedPage>
-      <motion.div className="page catalogue" initial="initial" animate="animate" exit="exit">
+      <motion.div className="page catalogue" initial="initial" exit="exit">
         <div className="content">
-          <h2>FAKEYYS GLASSES</h2>
-          <p>CHECKOUT OUR NEW RANGE OF LIMITED EDITION GLASSES.</p>
-          <p>EACH HAND MADE IN GERMANY BY OUR SKILLED CRAFTSMEN.</p>
+          <motion.div variants={textVariant} initial={'load'} animate={openModal ? 'hide' : 'start'} className="page-header">
+            <motion.h2>FAKEYYS GLASSES</motion.h2>
+            <motion.p>CHECKOUT OUR NEW RANGE OF LIMITED EDITION GLASSES.</motion.p>
+            <motion.p>EACH HAND MADE IN GERMANY BY OUR SKILLED CRAFTSMEN.</motion.p>
+          </motion.div>
           <div className="product-list">
               {fakeyysProducts.map(item => {
               const title = item.title.toUpperCase()
 
               return(
                 <>
-                {!openModal && <motion.div layoutId={item.id} onClick={()=>{setSelectedId(item.id); setSelectedProduct(item); setOpenModal(true)}}  className='product'>
+                <motion.div 
+                className='product'
+                variants={variants} 
+                animate={openModal ? 'hide' : 'show'} 
+                layoutId={item.id} 
+                onClick={()=>{setSelectedId(item.id); setSelectedProduct(item); setOpenModal(true); setLockScroll(true)}}  
+                >
                     <motion.div className='image-container'>
                       <motion.img src={item.cover_image}/>
                     </motion.div>
@@ -107,25 +145,32 @@ const catalogue = () => {
             
                         </motion.div>
                     </motion.div>
-                </motion.div>}
+                </motion.div>
                 </>
               )})}
 
-              {selectedId && selectedProduct && (
-                <motion.div onClick={()=>{setSelectedId(''); setSelectedProduct({}); setOpenModal(false)}} layoutId={selectedId} className="popup">
-                  <motion.div className='image-container'>
-                      <motion.img src={selectedProduct.cover_image}/>
-                    </motion.div>
-                    <motion.div className='content'>
-                        <motion.div className='head'>
-                          <motion.p>{selectedProduct.title}</motion.p>
-                          <motion.p>${selectedProduct.price}</motion.p>
-                        </motion.div>
-                        <motion.div className='foot'>
-            
-                        </motion.div>
-                    </motion.div>
-                </motion.div>
+              {selectedId && selectedProduct && openModal && (
+                <>
+                  <div onClick={() => {setOpenModal(false)}} className="backdrop"></div>
+                  <motion.div 
+                    className="popup"
+                    onClick={()=>{setSelectedId(''); setSelectedProduct({}); setOpenModal(false); setLockScroll(false)}} 
+                    layoutId={selectedId} 
+                  >
+                    <motion.div className='image-container'>
+                        <motion.img src={selectedProduct.cover_image}/>
+                      </motion.div>
+                      <motion.div className='content'>
+                          <motion.div className='head'>
+                            <motion.p>{selectedProduct.title}</motion.p>
+                            <motion.p>${selectedProduct.price}</motion.p>
+                          </motion.div>
+                          <motion.div className='foot'>
+              
+                          </motion.div>
+                      </motion.div>
+                  </motion.div>
+                </>
               )}
           <div className="product">
               <img src={'./images/comingSoon.png'}/>  
