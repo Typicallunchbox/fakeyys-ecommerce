957e1391f15e1dc6f6f777f8586d417e80fced1b
Keegan <keegan.launspach1@hotmail.com>
2023-08-22 15:43:01 +0200

    Add basic button for desktop and foot stlying

--- src/pages/catalogue.tsx
+++ src/pages/catalogue.tsx
@@ -34,13 +34,13 @@ const Catalogue = () => {
     }
 }, [lockScroll])
 
-  useEffect(() => {
-    if(selectedId){
-      window.history.pushState(null,'JavaScript',`/catalogue/${selectedId}`);
-    }else{
-      window.history.pushState(null,'JavaScript',`/catalogue`);
-    }
-  },[selectedProduct])
+  // useEffect(() => {
+  //   if(selectedId){
+  //     window.history.pushState(null,'JavaScript',`/catalogue/${selectedId}`);
+  //   }else{
+  //     window.history.pushState(null,'JavaScript',`/catalogue`);
+  //   }
+  // },[selectedProduct])
   
  
   function timeout(ms:number) {
@@ -137,9 +137,10 @@ const Catalogue = () => {
                             <motion.p className="product-title">{selectedProduct.title}</motion.p>
                             <motion.p>${selectedProduct.price}</motion.p>
                           </motion.div>
-                          <motion.div initial={{display: 'none', opacity:'0'}} animate={showModalContent ? {display: 'block', opacity:'1'} : {opacity:'0', display: 'none'}}  className='foot'>
+                          <motion.div initial={{display: 'none', opacity:'0'}} animate={showModalContent ? {display: 'flex', opacity:'1'} : {opacity:'0', display: 'none'}}  className='foot'>
                             <motion.p>{selectedProduct.description}</motion.p>
-                            <motion.p>${selectedProduct.price}</motion.p>
+                            {/* Add fabric texture animation on hover of button */}
+                            <motion.button>ADD TO CART</motion.button>
                           </motion.div>
                       </motion.div>
                   </motion.div>
